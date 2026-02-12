39d5274e9637fc39597a6ee540a4745794fd91ad
varkey-foodhub <geevarghese.regi@foodhub.com>
2026-02-12 17:27:11 +0530

    chore(refactor):Refactor menuEditor to src/features

---
+++ src/features/menu/components/MenuEditorPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const MenuEditorPage = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>MenuEditorPage</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default MenuEditorPage;
+
+const styles = StyleSheet.create({});
