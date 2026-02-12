6d0910e7bb921040d4006a15a9a41649cd3fca98
varkey-foodhub <geevarghese.regi@foodhub.com>
2026-02-12 12:46:23 +0530

    feat(navigation):Setup basic navigation outline

---
+++ app/(manager)/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+type Props = {};
+
+const HomeScreen = (props: Props) => {
+  return (
+    <SafeAreaView>
+      <Text>Restaurant Manager Home Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
