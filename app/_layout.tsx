14936b281c9396c4195bad098473667d0a5fd958
varkey-foodhub <geevarghese.regi@foodhub.com>
2026-02-12 16:44:59 +0530

    feat(react-query):Setup query client and added the provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,19 @@
+import { queryClient } from "@/src/lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import "react-native-reanimated";
-
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(manager)" options={{ headerShown: false }} />
-      <Stack.Screen name="menu" options={{ headerShown: false }} />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(manager)" options={{ headerShown: false }} />
+        <Stack.Screen name="menu" options={{ headerShown: false }} />
+      </Stack>
+    </QueryClientProvider>
   );
 }
---
+++ src/lib/queryClient.ts
@@ -0,0 +1,12 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      gcTime: 1000 * 60 * 10, // cache garbage collection
+      refetchOnWindowFocus: false,
+    },
+  },
+});
