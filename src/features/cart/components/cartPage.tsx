1f7820705632eecf31e8f7e7bc9ec804e0bebb3c
varkey-foodhub <geevarghese.regi@foodhub.com>
2026-02-13 14:42:23 +0530

    feat(cartPage): Created skeleton cart page

---
+++ src/features/cart/components/cartPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+type Props = {};
+
+const CartPage = (props: Props) => {
+  return (
+    <View>
+      <Text>CartPage</Text>
+    </View>
+  );
+};
+
+export default CartPage;
+
+const styles = StyleSheet.create({});
