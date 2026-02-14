e86eb9e1bd1e01c7bce2645132affee670eada21
varkey-foodhub <geevarghese.regi@foodhub.com>
2026-02-14 15:09:31 +0530

    feat(statusBar):Autocolor statusbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,9 @@ import { AppToastConfig } from "@/src/components/ui/AppToast";
 import { queryClient } from "@/src/lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { useColorScheme } from "react-native";
 import "react-native-reanimated";
 import Toast from "react-native-toast-message";
 
@@ -11,17 +13,22 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
-  const toastConfig = AppToastConfig(); // ✅ inside component
+  const toastConfig = AppToastConfig();
+  const colorScheme = useColorScheme();
+
+  const isDark = colorScheme === "dark";
 
   return (
     <QueryClientProvider client={queryClient}>
+      {/* ✅ Auto theme status bar */}
+      <StatusBar style={isDark ? "light" : "dark"} />
+
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(manager)" options={{ headerShown: false }} />
         <Stack.Screen name="menu" options={{ headerShown: false }} />
       </Stack>
 
-      {/* Always mount AFTER navigation */}
       <Toast config={toastConfig} />
     </QueryClientProvider>
   );
